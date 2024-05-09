02fec0183bd73963332fb6796d12f0c13b7fce29
Michael Aldridge <aldridge.mac@gmail.com>
2024-05-09 17:42:29 -0500

    pkg/docs: Include option to just not have docs

---
+++ pkg/docs/docs_not_included.go
@@ -0,0 +1,20 @@
+//go:build !docs
+package docs
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type handler struct{}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Documentation is not available in this build!")
+}
+
+// Handler just returns a handler that no matter what you put into it
+// it tells you the docs aren't included in your current build.
+func Handler() http.Handler {
+	h := new(handler)
+	return h
+}
