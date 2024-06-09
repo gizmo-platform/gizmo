146cb779e3dc31859c91f837aa36540356886b26
Michael Aldridge <aldridge.mac@gmail.com>
2024-06-09 02:16:20 -0500

    pkg/ds: Remove really broken FMS detection logic

--- pkg/ds/ds.go
+++ pkg/ds/ds.go
@@ -1,10 +1,8 @@
 package ds
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"net"
 	"path"
 	"strconv"
 	"time"
@@ -39,28 +37,9 @@ func New(opts ...Option) *DriverStation {
 // no external FMS is detected then it will also run the bare minimum
 // of FMS services locally.
 func (ds *DriverStation) Run() error {
-	for !ds.quit {
-		ds.stop = make(chan struct{})
-		go ds.findFMS()
-		if !ds.fmsAvailable {
-			// We're running standalone so spin up the local
-			// broker and location service.
-			go ds.doLocalBroker()
-			go ds.doLocation()
-
-			// Bring up the local radio
-			ds.svc.Start("hostapd")
-		} else {
-			// The FMS exists at a special DNS alias, if that
-			// resolved, we can connect to it.
-			ds.connectMQTT("mqtt://fms.gizmo:1883")
-
-			// Shut down the local radio
-			ds.svc.Stop("hostapd")
-		}
-		go ds.doGamepad()
-		<-ds.stop
-	}
+	go ds.doLocalBroker()
+	go ds.doLocation()
+	ds.doGamepad()
 	return nil
 }
 
@@ -70,30 +49,6 @@ func (ds *DriverStation) Stop() {
 	ds.l.Info("Receieved stop request")
 	ds.quit = true
 	close(ds.stop)
-}
-
-func (ds *DriverStation) findFMS() {
-	var r net.Resolver
-	t := time.NewTicker(time.Second * 5)
-	for {
-		select {
-		case <-ds.stop:
-			t.Stop()
-		case <-t.C:
-			ctx, _ := context.WithTimeout(context.Background(), time.Millisecond*50)
-			_, err := r.LookupHost(ctx, "nxdomain.example.com")
-			dnsWrong := err == nil
-			_, err = r.LookupHost(ctx, "fms.gizmo")
-			ds.l.Trace("FMS Detection result", "error", err)
-			available := (err == nil && !dnsWrong)
-
-			if ds.fmsAvailable != available {
-				ds.l.Debug("FMS Availability changed", "available", available)
-				close(ds.stop)
-			}
-			ds.fmsAvailable = available
-		}
-	}
 }
 
 func (ds *DriverStation) connectMQTT(address string) error {
