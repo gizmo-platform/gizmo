2f80e36e52125eb04b3ed1c979ebe69484a0012a
Michael Aldridge <aldridge.mac@gmail.com>
2025-03-09 23:27:46 -0500

    pkg/fms: Make config reloadable

--- pkg/fms/config.go
+++ pkg/fms/config.go
@@ -3,6 +3,8 @@ package fms
 import (
 	"encoding/json"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/hashicorp/go-hclog"
 )
@@ -19,24 +21,41 @@ func NewConfig(l hclog.Logger) (*Config, error) {
 	}
 
 	c := new(Config)
+	c.l = l.Named("config")
 	c.Teams = make(map[int]*Team)
 	c.Fields = make(map[int]*Field)
 	c.path = os.Getenv("GIZMO_FMS_CONFIG")
 	if c.path == "" {
 		c.path = defconfPath
 	}
+	c.l.Debug("config path set", "path", c.path)
 
-	c.l = l.Named("config")
-	if err := c.Load(c.path); err != nil {
+	if err := c.Load(); err != nil {
 		return c, err
 	}
+
+	// If we made it here then the config loaded, so we'll go
+	// ahead and setup the reloader so that its possible to update
+	// the config again later.
+	rChan := make(chan os.Signal, 1)
+	signal.Notify(rChan, syscall.SIGHUP)
+
+	go func() {
+		for {
+			<-rChan
+			c.l.Debug("Config reload requested")
+			if err := c.Load(); err != nil {
+				c.l.Warn("Error reloading config", "error", err)
+			}
+		}
+	}()
 
 	return c, nil
 }
 
 // Load loads a config file from the given path on disk.
-func (c *Config) Load(path string) error {
-	f, err := os.Open(path)
+func (c *Config) Load() error {
+	f, err := os.Open(c.path)
 	if err != nil {
 		return err
 	}
