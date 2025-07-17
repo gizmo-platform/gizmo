6defdb841902c529d80775fd4b73b0eacb2be23d
Michael Aldridge <aldridge.mac@gmail.com>
2025-07-16 23:29:15 -0500

    pkg/eventstream: Add null event stream implementation

---
+++ pkg/eventstream/null.go
@@ -0,0 +1,20 @@
+package eventstream
+
+// NullStream doesn't publish events anywhere and is mostly for
+// testing or non-server CLI cmdlets.
+type NullStream struct{}
+
+// NewNullStreamer hands back a null stream instance that discards
+// everything.
+func NewNullStreamer() *NullStream {
+	return new(NullStream)
+}
+
+// PublishError discards all errors.
+func (ns *NullStream) PublishError(_ error) {}
+
+// PublishLogLine discards all log lines.
+func (ns *NullStream) PublishLogLine(_ string) {}
+
+// PublishFileFetch discards all filenames.
+func (ns *NullStream) PublishFileFetch(_ string) {}
