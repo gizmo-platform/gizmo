5e725056c8d40a5b057331736067a652b96d86a6
Michael Aldridge <aldridge.mac@gmail.com>
2025-10-01 15:32:21 -0500

    pkg/docs: Support loading docs from external bundle

--- pkg/docs/docs.go
+++ pkg/docs/docs.go
@@ -10,6 +10,7 @@ import (
 	"embed"
 	"io/fs"
 	"net/http"
+	"os"
 )
 
 //go:generate mdbook build mdbook
@@ -20,7 +21,10 @@ var efs embed.FS
 // MakeHandler returns the contents of the embedded docs filesystem.
 func MakeHandler(path string) http.Handler {
 	return func() http.Handler {
-		efs, _ := fs.Sub(efs, "mdbook/book/html")
-		return http.StripPrefix(path, http.FileServer(http.FS(efs)))
+		docFS, _ := fs.Sub(efs, "mdbook/book/html")
+		if os.Getenv("GIZMO_DOCS") != "" {
+			docFS = os.DirFS(os.Getenv("GIZMO_DOCS"))
+		}
+		return http.StripPrefix(path, http.FileServer(http.FS(docFS)))
 	}()
 }
--- pkg/fms/system/tpl/gizmo-fms.run.tpl
+++ pkg/fms/system/tpl/gizmo-fms.run.tpl
@@ -7,5 +7,6 @@ cd /var/lib/gizmo || exit 1
 [ -f .htgroup ] || touch .htgroup
 export USER=_gizmo
 export HOME=/var/lib/gizmo
+export GIZMO_DOCS=/usr/share/gizmo/docs/html
 exec 2>&1
 exec chpst -u _gizmo:wheel:dialout /usr/bin/gizmo fms run
